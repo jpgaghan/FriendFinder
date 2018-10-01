fbc006d3621ecd47143c2117cd34a0cc5cddba4e
Jonathan Gaghan <jpgaghan@gmail.com>
2018-10-01 18:18:49 -0400

    done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,23 +2,23 @@ const express = require("express");
 
 const app = express();
 var path =require("path")
-const friends = require('../data/friends');
-
-
+const friends = require('../data/friends.js');
 
 module.exports = 
   function (app) {
     app.get("/api/friends", function(req, res) {
       return res.json(friends);
     });
+    
+    app.post("/api/friends", function(req, res) {
+      console.log("hello");
+      newFriend = req.body;
+      console.log(newFriend)
+      friends.push(newFriend);
+      res.json(newFriend);
+    });
   }
 
-app.post("/api/friends", function(req, res) {
-  const newFriend = req.body;
-  Friends.push(newFriend);
-  res.json(newFriend);
-});
-
 function findFriend(friends) {
-  console.log(newFriend.score.reduce(accum,0))
+  console.log(newFriend.score.reduce((accum,currentValue) => accum + currentValue, 0))
 }
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
+
 const friends = require('./app/data/friends.js');
 
 console.log(friends)
@@ -13,25 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-// app.post("/api/friends", function (req, res) {
-//   const newFriend = req.body;
-//   Friends.push(newFriend);
-//   res.json(newFriend);
-// });
-
-// app.get("/api/friends", function (req, res) {
-//   return res.json(friends);
-// });
-
-// app.get("/survey", function (req, res) {
-//   res.sendFile(path.join(__dirname, "app/public/survey.html"));
-// });
-
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, "app/public/home.html"));
-// });
-
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT" + PORT);
